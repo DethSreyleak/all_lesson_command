18e56a6dd9c50c590fee3d8dc18a080a11b839b1
sreyleak <deth.sereyleak19@kit.edu.kh>
2021-12-18 11:03:27 +0700

    command lesson

---
+++ How to create project in Vue.js
@@ -0,0 +1,14 @@
+How to create project in vue.js
+1. vue create mevn-stack( hello_world it just folder name you can change any name that you like)
+2. choose word: Manually select features
+3. choose word:  - Babel
+		 - Router
+		 - Vuex
+		 - Linter/formatter
+4. choose word: ESLint with error prevention only
+5. choose word: Lint on save
+6. choose word: In dedicated config files
+7. and type word: no
+8. go to folder name mevn-stack type: cd mevn-stack (mevn-stack it just folder name so you can change any name fild that you like)
+9.  to run code type: npm run serve
+10. to open folder in VS code type: code .
---
+++ how to create project using React.js
@@ -0,0 +1,4 @@
+How to create project by using React.js
+
+1. npx create-react-app react-website-1 (react-websit-1, it just name. so you can change name that you want to put)
+2. 
